2b5ba30f5c2ab7001632ce4dfb3b376aed4e2ac3
Ronny Quispe <ronny.quispe@udla.edu.ec>
2024-01-17 23:10:55 -0500

    aumentamos las librerias

--- alumno.c
+++ alumno.c
@@ -1,7 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 struct alumno {
     int matricula;
     char nombre[50];
     char direccion[50];
     char carrera[50];
     float promedio;
-};
+}
+;
