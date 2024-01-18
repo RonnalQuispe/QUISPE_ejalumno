c3cf3c0123698285180cb7d2feaecc30780c1b21
Ronny Quispe <ronny.quispe@udla.edu.ec>
2024-01-17 23:14:46 -0500

    La función ingresarDatosse encarga de solicitar al usuario que ingrese los datos de los alumnos y almacenarlos en un arreglo de estructuras alumno. Toma dos argumentos: un puntero a un arreglo de estructuras alumnoy el número de alumnos n:

--- alumno.c
+++ alumno.c
@@ -9,3 +9,20 @@ struct alumno {
     float promedio;
 }
 ;
+void ingresarDatos(struct alumno *alumnos, int n) {
+    for (int i = 0; i < n; i++) {
+        printf("\n");
+        printf("Ingrese los datos del alumno %d:\n", i+1);
+        printf("Matricula: ");
+        scanf("%d", &(alumnos[i].matricula));
+        printf("Nombre: ");
+        scanf("%s", alumnos[i].nombre);
+        printf("Direccion: ");
+        scanf("%s", alumnos[i].direccion);
+        printf("Carrera: ");
+        scanf("%s", alumnos[i].carrera);
+        printf("Promedio: ");
+        scanf("%f", &(alumnos[i].promedio));
+        getchar(); // Limpiar el teclado
+    }
+}
